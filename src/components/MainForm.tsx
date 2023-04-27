219f210d4d64638a3f39090416179a3c4fe8baa2
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-27 18:09:49 +0530

    components for main form

---
+++ src/components/MainForm.tsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import ColorPicker from "./ColorPicker";
+import React from "react";
+
+const MainForm = () => {
+  return (
+    <div>
+      <Button />
+      <ColorPicker />
+    </div>
+  );
+};
+
+export default MainForm;
