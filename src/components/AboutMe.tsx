df51642434ade5cfeb677408ad4e9165227fde0d
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-27 18:10:03 +0530

    placeholder for note

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,18 +2,43 @@ import Divider from "./Divider";
 
 const AboutMe = () => {
   const about =
-    "   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe necessitatibus perspiciatis maiores quidem aperiam eligendi consectetur, architecto velit, optio eum placeat esse possimus reprehenderit quis commodi atque. Delectus, obcaecati labore!";
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore quasi, at,  udantium sapiente quia enim totam. Nihil ";
 
   const aboutText = about.replace(/^\s+/, "");
 
   if (aboutText.length === 0) {
-    return <div className="emptyDiv"></div>;
+    return (
+      <>
+        <div className="noteContainerNon">note</div>
+        <form>
+          <label>
+            <input
+              className="formNon"
+              type="text"
+              name="name"
+              placeholder="click to add note"
+            />
+          </label>
+        </form>
+      </>
+    );
   } else {
     return (
       <>
         <div className="aboutMeContainer">
           <div className="aboutMeHeading">About Me</div>
           <div className="aboutMeContent">{aboutText}</div>
+          <div className="noteContainer">note</div>
+          <form>
+            <label>
+              <input
+                className="form"
+                type="text"
+                name="name"
+                placeholder="click to add note"
+              />
+            </label>
+          </form>
         </div>
       </>
     );
