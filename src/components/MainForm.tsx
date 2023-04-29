509866cbf56cc767f66ceb9bc1f04ae9a51068a0
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-29 22:59:24 +0530

    added forms

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,12 +1,38 @@
 import Button from "./Button";
 import ColorPicker from "./ColorPicker";
 import React from "react";
+import Divider from "./Divider";
 
 const MainForm = () => {
   return (
-    <div>
-      <Button />
-      <ColorPicker />
+    <div className="formCard">
+      <div className="profileThemeContainer">
+        <div className="smallHeaderText grey-text">profile theme</div>
+        <div className="colorPickerContainer">
+          <div className="primaryPickerButton">
+            <ColorPicker />
+            <div className="colorPickerText">Primary</div>
+          </div>
+          <div className="accentPickerButton">
+            <ColorPicker />
+            <div className="colorPickerText">Accent</div>
+          </div>
+        </div>
+        <Divider />
+        <div className="profileBannerContainer">
+          <div className="smallHeaderText grey-text">profile banner</div>
+          <div className="profileBannerButton">
+            <Button type="primary">Change Banner</Button>
+          </div>
+        </div>
+        <Divider />
+        <div className="profileAvatarContainer">
+          <div className="smallHeaderText grey-text">animated avatar</div>
+          <div className="profileAvatarButton">
+            <Button type="primary">Change Avatar</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
