32cad3720a28a6e94e1ccdc6056c6ba3376969b4
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-29 22:59:54 +0530

    added color picker component

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ChromePicker } from "react-color";
 
-const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+const ColorPicker = ({ handleOutsideClick }: any) => {
+  const [color, setColor] = useState("#000000");
+  const [showColorPicker, setShowColorPicker] = useState(false);
+
+  const handleClick = () => {
+    setShowColorPicker(!showColorPicker);
+  };
+
+  const handleChange = (updatedColor: any) => {
+    setColor(updatedColor.hex);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (showColorPicker && !event.target.closest(".colorPickerButton")) {
+        setShowColorPicker(false);
+        handleOutsideClick(); // Call the callback passed as prop
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    const handleKeyDown = (event: any) => {
+      if (event.key === "Escape") {
+        setShowColorPicker(false);
+        handleOutsideClick(); // Call the callback passed as prop
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [showColorPicker, handleOutsideClick]);
+
+  return (
+    <div>
+      <button
+        className="colorPickerButton"
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
+      ></button>
+
+      {showColorPicker && (
+        <ChromePicker color={color} onChange={handleChange} />
+      )}
+    </div>
+  );
 };
 
 export default ColorPicker;
