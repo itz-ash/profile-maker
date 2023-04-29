7dc43fe9400dfee47c725a08709d2eb6b1d4ac0a
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-29 22:59:40 +0530

    added button componen

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+interface Props {
+  children: ReactNode;
+  customStyle?: React.CSSProperties;
+  type?: "primary" | "secondary" | "grey";
+}
+
+const Button = (props: Props) => {
+  const backgroundColorClass =
+    props.type === "primary"
+      ? "blue"
+      : props.type === "secondary"
+      ? "green"
+      : "grey";
+
+  return (
+    <button
+      className={"formButton " + backgroundColorClass}
+      style={{ ...props.customStyle }}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
