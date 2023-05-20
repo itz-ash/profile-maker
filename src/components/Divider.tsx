e6a2cbe198389adf36c51c03d91b21b27c44314a
ItzNotAsh <chawlaashneet@gmail.com>
2023-05-21 02:20:38 +0530

    deleted

--- src/components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-import "../styles/style.css";
-
-const Divider = () => {
-  return <div className="divider"></div>;
-};
-
-export default Divider;
--- src/react-click-outside.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "react-click-outside" {
-  const onClickOutside: any;
-  export default onClickOutside;
-}
