f645a6832d2795648f8fc58048e26dc039c58dec
ItzNotAsh <chawlaashneet@gmail.com>
2023-06-11 22:13:09 +0530

    ee

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./layouts/App"
+import App from "./App"
 import { Provider } from "react-redux"
 import { store } from "./state/store"
+import { BrowserRouter as Router } from "react-router-dom"
+import { MantineProvider } from "@mantine/core"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <MantineProvider withGlobalStyles withNormalizeCSS> */}
+    <MantineProvider>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </MantineProvider>
   </React.StrictMode>
 )
